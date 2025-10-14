3a76a50a9556ce478ce90ef6c83b8f09653b6de1
o_leo <cchaeyoon1313@gmail.com>
2025-10-15 00:15:05 +0900

    script.js 업데이트

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ fetch('posts.json')
   .then(posts => {
     const container = document.getElementById('feed');
 
-    // 날짜 기준 내림차순 정렬 (최근 게시물이 위)
+    // 최신 날짜 기준 내림차순 정렬
     posts.sort((a, b) => new Date(b.date) - new Date(a.date));
 
     posts.forEach(post => {
@@ -17,6 +17,4 @@ fetch('posts.json')
       container.appendChild(div);
     });
   })
-  .catch(err => {
-    console.error('게시물 불러오기 오류:', err);
-  });
+  .catch(err => console.error('게시물 불러오기 오류:', err));
